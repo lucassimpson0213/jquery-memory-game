4a247eca6732721895fe53d283aaa507c16681db
lucassimpson0213 <113938275+lucassimpson0213@users.noreply.github.com>
2024-02-19 14:40:55 -0600

    testing to see if changing rows will work in select

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
 $(document).ready(function () {
     $('#tabs').tabs();
-    $('#row1').hide();
+    
+
+
+    let rows = new Map([
+            [8, 1],
+            [16, 2],
+            [24, 3],
+            [32, 4],
+            [40, 5],
+            [48, 6]
+        
+        ]
+    )
 
 
 
     $('#num_cards').on('change', function () {
         var val = $(this).val();
-        console.log(val);
+        var numRows = rows.get(val);
+        var amtRows = getRows(rows);
+
+        for(let i = 0; i <= amtRows; i++){
+            $(`#row${i}`).hide();
+        }
+
 
     })
 
 
 
 });
+
+
+function getRows(cardsAmt) {
+    return 6 - cardsAmt;
+}
 
