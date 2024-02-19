4d382725fb0542988aa7aec59aef4b28b90eec7f
lucassimpson0213 <113938275+lucassimpson0213@users.noreply.github.com>
2024-02-19 14:15:25 -0600

    testing whether on change event works for num cards

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 $(document).ready(function () {
     $('#tabs').tabs();
     $('#row1').hide();
+
+
+
+    $('#num_cards').on('change', function () {
+           var e =  $('#num_cards').val;
+           console.log(e);
+
+    })
+
+
    
 });
 
