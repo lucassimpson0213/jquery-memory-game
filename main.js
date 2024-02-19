01fbeb446b1583213cfff36ee9eb136664a577c7
lucassimpson0213 <113938275+lucassimpson0213@users.noreply.github.com>
2024-02-18 19:56:59 -0600

    seeing if removing this helps

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 $(document).ready(function () {
     $('#tabs').tabs();
 
-    $('#row1').show();
-    $('#row2').show();
-    $('#row3').show();
-    $('#row4').show();
-    $('#row5').show();
-    $('#row6').show();
-
-    $('#row6').hide();
-    $('#row5').hide();
+   
 });
 
