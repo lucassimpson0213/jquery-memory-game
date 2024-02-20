17feabe3a99e9323bb0a662ee6ad2d581993305f
lucassimpson0213 <113938275+lucassimpson0213@users.noreply.github.com>
2024-02-20 14:27:00 -0600

    fixed tabbing and index

--- main.js
+++ main.js
@@ -1,51 +1,33 @@
 $(document).ready(function () {
     $('#tabs').tabs();
-    
-
 
     let rowsMap = new Map([
-            [8, 1],
-            [16, 2],
-            [24, 3],
-            [32, 4],
-            [40, 5],
-            [48, 6]
-        
-        ]
-    )
-
-
+        [8, 1],
+        [16, 2],
+        [24, 3],
+        [32, 4],
+        [40, 5],
+        [48, 6]
+    ]);
 
     $('#num_cards').on('change', function () {
         var val = parseInt($(this).val(), 10);
-        var requiredRows = rowsMap.get(val)
-        
+        var requiredRows = rowsMap.get(val);
 
-        for(let i = 1; i <= 6; i++){
-            if(i <= requiredRows) 
-            {
+        for (let i = 1; i <= 6; i++) {
+            if (i <= requiredRows) {
                 $('#row' + i).show();
             } else {
-
                 $('#row' + i).hide();
             }
         }
-
-
-    })
-
-      $('a').each(function (index, element) {
-         console.log(`Jquery Element ${i}: \n \n ${element}`)
-      })
-        
     });
-    console.log(value)
 
-
-
-
-
-
+    $('a').each(function (index, element) {
+        // Fixed variable in the template string from `i` to `index`
+        console.log(`Jquery Element ${index}: \n \n`, element); // Also, separated the element from the template string for better logging
+    });
 });
+
 
 
