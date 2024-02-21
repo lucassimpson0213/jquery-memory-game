e16eecbad2e4b2e3b1cb757e6b6f0ba2e56e59e6
lssimp1 <lucassimpson0213@gmail.com>
2024-02-21 16:10:52 -0600

    updating my code to encapsulate logic

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
 $(document).ready(function () {
-    $('#tabs').tabs();
+    function initTabs() {
+        $('#tabs').tabs();
+    }
 
-    let rowsMap = new Map([
-        [8, 1],
-        [16, 2],
-        [24, 3],
-        [32, 4],
-        [40, 5],
-        [48, 6]
-    ]);
-
-    $('#num_cards').on('change', function () {
-        var val = parseInt($(this).val(), 10);
-        var requiredRows = rowsMap.get(val);
-
+    function adjustRowsVisibility(requiredRows) {
         for (let i = 1; i <= 6; i++) {
             if (i <= requiredRows) {
                 $('#row' + i).show();
@@ -21,18 +11,32 @@ $(document).ready(function () {
                 $('#row' + i).hide();
             }
         }
+    }
+
+    function updateRowsVisibility() {
+        let rowsMap = new Map([
+            [8, 1],
+            [16, 2],
+            [24, 3],
+            [32, 4],
+            [40, 5],
+            [48, 6]
+        ]);
+
+        var val = parseInt($('#num_cards').val(), 10);
+        var requiredRows = rowsMap.get(val);
+        adjustRowsVisibility(requiredRows);
+    }
+
+    function logAnchorTags() {
+        $('a').each(function (index, element) {
+            console.log(`Jquery Element ${index}:`, element);
+        });
+    }
+
+    $('#save_settings').click(function () {
+        updateRowsVisibility();
     });
 
-    $('a').each(function (index, element) {
-        // Fixed variable in the template string from `i` to `index`
-        console.log(`Jquery Element ${index}: \n \n`, element); // Also, separated the element from the template string for better logging
-    });
-
-
-
-
-    
+    initTabs();
 });
-
-
-
