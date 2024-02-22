0b42fd33bcf4ec37aec3a9744845acec6def13e0
lucassimpson0213 <113938275+lucassimpson0213@users.noreply.github.com>
2024-02-21 21:43:12 -0600

    pushing to test

--- main.js
+++ main.js
@@ -1,98 +1,40 @@
 $(document).ready(function () {
-
-
     const uniqueImages = [
+        "/images/card_1.png",
+        "/images/card_2.png",
+        "/images/card_3.png",
+        "/images/card_4.png",
+        "/images/card_5.png",
+        "/images/card_6.png",
+        "/images/card_7.png",
+        "/images/card_8.png",
+        "/images/card_9.png",
+        "/images/card_10.png",
+        "/images/card_11.png",
+        "/images/card_12.png",
+        "/images/card_13.png",
+        "/images/card_14.png",
+        "/images/card_15.png",
+        "/images/card_16.png",
+        "/images/card_17.png",
+        "/images/card_18.png",
+        "/images/card_19.png",
+        "/images/card_20.png",
+        "/images/card_21.png",
+        "/images/card_22.png",
+        "/images/card_23.png",
+        "/images/card_24.png"
+    ];
 
-                "/images/card_1.png",
-
-                "/images/card_2.png",
-
-                "/images/card_3.png",
-
-                "/images/card_4.png",
-
-                "/images/card_5.png",
-
-                "/images/card_6.png",
-
-                "/images/card_7.png",
-
-                "/images/card_8.png",
-
-                "/images/card_9.png",
-
-                "/images/card_10.png",
-
-                "/images/card_11.png",
-
-                "/images/card_12.png",
-
-                "/images/card_13.png",
-
-                "/images/card_14.png",
-
-                "/images/card_15.png",
-
-                "/images/card_16.png",
-
-                "/images/card_17.png",
-
-                "/images/card_18.png",
-
-                "/images/card_19.png",
-
-                "/images/card_20.png",
-
-                "/images/card_21.png",
-
-                "/images/card_22.png",
-
-                "/images/card_23.png",
-
-                "/images/card_24.png"
-
-            
-
-    ]
-
-    
-
-
-
-    // Initializes jQuery UI tabs
     function initUITabs() {
         $('#tabs').tabs();
     }
 
-    const preloadedImages = [];
-
     function preloadImages() {
-        uniqueImages.forEach(function(src) {
+        uniqueImages.forEach(src => {
             const img = new Image();
             img.src = src;
-            preloadedImages.push(img);
         });
-    }
-
-    let cards = []; // Use this array to track cards for the game
-
-    function prepareCards(amountCards) {
-        let cardsMap = new Map([
-            [8, 4],
-            [16, 8],
-            [24, 12],
-            [32, 16],
-            [40, 20],
-            [48, 24]
-        ]);
-
-        let numberOfUniqueCardsNeeded = cardsMap.get(amountCards);
-        cards = uniqueImages.slice(0, numberOfUniqueCardsNeeded).flatMap(src => [src, src]);
-        shuffle(cards);
-    }
-
-    function shuffle(array) {
-        array.sort(() => Math.random() - 0.5);
     }
 
     function GetFromSessionStorage() {
@@ -114,15 +56,12 @@ $(document).ready(function () {
 
     function adjustRows(requiredRows) {
         for (let i = 1; i <= 6; i++) {
-            if (i <= requiredRows) {
-                $('#row' + i).show();
-            } else {
-                $('#row' + i).hide();
-            }
+            $('#row' + i).toggle(i <= requiredRows);
         }
     }
 
     function updateRows() {
+        let selectedNumberOfCards = parseInt($('#num_cards').val(), 10);
         let rowsMap = new Map([
             [8, 1],
             [16, 2],
@@ -131,29 +70,17 @@ $(document).ready(function () {
             [40, 5],
             [48, 6]
         ]);
-
-        let selectedNumberOfCards = parseInt($('#num_cards').val(), 10);
-        prepareCards(selectedNumberOfCards); // This needs to adjust based on the actual value
-        let requiredRows = rowsMap.get(selectedNumberOfCards); // Fixed the undefined variable by using the selected number of cards
+        let requiredRows = rowsMap.get(selectedNumberOfCards);
         adjustRows(requiredRows);
     }
 
-    function logAnchorTags() {
-        $('a').each(function(index, element) {
-            console.log(`Jquery Element ${index}:`, element);
-        });
-    }
-
-    $('#save_settings').click(function() {
+    $('#save_settings').click(function () {
         updateRows();
         addToSessionStorage();
     });
 
-    // Initial setup
     preloadImages();
     initUITabs();
     GetFromSessionStorage();
-    prepareCards(48); // Preparing for the default case of 48 cards
-    
-    // Display the player's name from sessionStorage if available
+    updateRows(); // Call updateRows to adjust the UI based on the default or previously saved settings
 });
