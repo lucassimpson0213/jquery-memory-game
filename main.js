437c52c1ce7a6b7594c721a53e135ee9abf89c36
lssimp1 <lucassimpson0213@gmail.com>
2024-02-22 22:36:09 -0600

    [ushing to see if I can fix the high score updated

--- main.js
+++ main.js
@@ -2,30 +2,14 @@ $(document).ready(function () {
 
     // Array containing paths to unique images
     const uniqueImages = [
-        "./images/card_1.png",
-        "./images/card_2.png",
-        "./images/card_3.png",
-        "./images/card_4.png",
-        "./images/card_5.png",
-        "./images/card_6.png",
-        "./images/card_7.png",
-        "./images/card_8.png",
-        "./images/card_9.png",
-        "./images/card_10.png",
-        "./images/card_11.png",
-        "./images/card_12.png",
-        "./images/card_13.png",
-        "./images/card_14.png",
-        "./images/card_15.png",
-        "./images/card_16.png",
-        "./images/card_17.png",
-        "./images/card_18.png",
-        "./images/card_19.png",
-        "./images/card_20.png",
-        "./images/card_21.png",
-        "./images/card_22.png",
-        "./images/card_23.png",
-        "./images/card_24.png"
+        "./images/card_1.png", "./images/card_2.png", "./images/card_3.png",
+        "./images/card_4.png", "./images/card_5.png", "./images/card_6.png",
+        "./images/card_7.png", "./images/card_8.png", "./images/card_9.png",
+        "./images/card_10.png", "./images/card_11.png", "./images/card_12.png",
+        "./images/card_13.png", "./images/card_14.png", "./images/card_15.png",
+        "./images/card_16.png", "./images/card_17.png", "./images/card_18.png",
+        "./images/card_19.png", "./images/card_20.png", "./images/card_21.png",
+        "./images/card_22.png", "./images/card_23.png", "./images/card_24.png"
     ];
 
     let selectedImages = [];
@@ -72,16 +56,18 @@ $(document).ready(function () {
 
     // Calculate final score and update high score if necessary
     function calculateFinalScore() {
-        let currentScore = correctClicks / amtClicks; // Example score calculation
-        let highScore = localStorage.getItem('highScore') ? parseFloat(localStorage.getItem('highScore')) : 0;
+        let currentScore = correctClicks / amtClicks;
+        let highScore = localStorage.getItem('highScore') ? (parseFloat(localStorage.getItem('highScore')) * 100) : 0;
 
         if (currentScore > highScore) {
             localStorage.setItem('highScore', currentScore.toString());
-            console.log("New high score:", currentScore);
+            console.log("New high score:", currentScore * 100);
+            // Reset clicks for a new game session
             amtClicks = 0;
             correctClicks = 0;
         }
     }
+
 
     // Array to store flipped cards and tracking clicks
     let flippedCards = [];
@@ -189,12 +175,7 @@ $(document).ready(function () {
     function updateRows() {
         let selectedNumberOfCards = parseInt($('#num_cards').val(), 10);
         let rowsMap = new Map([
-            [8, 1],
-            [16, 2],
-            [24, 3],
-            [32, 4],
-            [40, 5],
-            [48, 6]
+            [8, 1], [16, 2], [24, 3], [32, 4], [40, 5], [48, 6]
         ]);
         let requiredRows = rowsMap.get(selectedNumberOfCards);
         adjustRows(requiredRows);
